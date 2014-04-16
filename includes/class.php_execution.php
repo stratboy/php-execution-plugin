781657514c5a405bcaa93c44cb66d6b115d7d86c
stratboy <email@reghellin.com>
2014-04-16 16:04:04 +0200

    fixed deprecated \e modifier > preg_replace to preg_replace_callback

--- includes/class.php_execution.php
+++ includes/class.php_execution.php
@@ -11,27 +11,27 @@ class php_execution
 
 	/**
 	 * cache array for $user = new WP_User($id); $user->has_cap(PHP_EXECUTION_CAPABILITY);
-	 */	
+	 */
 	var $cap_cache = array();
-	
+
 	/**
 	 * constructor
 	*/
 	function php_execution()
 	{
 		/*
-		 * TinyMce hook - load js file 
+		 * TinyMce hook - load js file
 		 * bug fix -> moved outside "if(WP_ADMIN)" for compatibility with WP version 2.5
 		 */
 		add_action('mce_external_plugins', array(&$this,'action_mce_external_plugins'));
-		
+
 		global $pagenow;
-		
+
 		if ( $pagenow && in_array( $pagenow, array('post.php', 'post-new.php', 'page.php', 'page-new.php') ) )
 		{
 			add_action( 'admin_print_scripts', array(&$this,'action_admin_print_scripts') );
 		}
-		
+
 		if( defined('WP_ADMIN') )
 		{
 			// installation
@@ -41,10 +41,10 @@ class php_execution
 
 			// Administration Area
 			add_action('admin_menu', array(&$this,'action_admin_menu'));
-			
+
 			// TinyMce hooks
 			add_action('the_editor_content', array(&$this,'action_the_editor_content'), 1);
-			
+
 			// init
 			add_action('init', array(&$this,'action_init'));
 		}
@@ -53,7 +53,7 @@ class php_execution
 			// content hooks
 			add_action('the_content', array(&$this,'action_the_content'), 1);
 			add_action('the_content_rss', array(&$this, 'action_the_content'), 1);
-			
+
 			add_action('the_excerpt', array(&$this, 'action_the_content'), 1);
 			add_action('the_excerpt_rss', array(&$this, 'action_the_content'), 1);
 		}
@@ -65,12 +65,12 @@ class php_execution
 	function action_activate()
 	{
 		global $wp_roles;
-		
+
 		if( $role = $wp_roles->get_role('administrator') )
 		{
 			$role->add_cap(PHP_EXECUTION_CAPABILITY);
 		}
-		
+
 		add_option(PHP_EXECUTION_OPTION, array('observer_active'=>1));
 	}
 
@@ -80,12 +80,12 @@ class php_execution
 	function action_deactivate()
 	{
 		global $wp_roles;
-		
+
 		if( $role = $wp_roles->get_role('administrator') )
 		{
 			$role->remove_cap(PHP_EXECUTION_CAPABILITY);
 		}
-		
+
 		delete_option(PHP_EXECUTION_OPTION);
 	}
 
@@ -98,11 +98,11 @@ class php_execution
 		 * bug fix(1.0.1) -> don't use $current_user global as it might not already be created
 		 */
 		$current_user = wp_get_current_user();
-		
+
 		$this->current_user_can_exec_php = (!empty($current_user) && $current_user->has_cap(PHP_EXECUTION_CAPABILITY));
-		
+
 		$this->options = get_option(PHP_EXECUTION_OPTION);
-		
+
 		if(!$this->current_user_can_exec_php && $this->options['observer_active'])
 		{
 			// add Post Edit Monitor
@@ -132,13 +132,13 @@ class php_execution
 			{
 				case($args[0] == 'edit_post' && in_array('edit_others_posts',$caps)):
 				case($args[0] == 'edit_page' && in_array('edit_others_pages',$caps)):
-					
+
 					$author_can_exec_php = true; // till better info we expect the author to have rights to execute php code
-					
+
 					if( $args[2] && ($post = get_post($args[2])) ) // if postID is specified => check post->authors php execution rights
 					{
 						$id = $post->post_author;
-						
+
 						if(isset($this->cap_cache[$id]))
 						{
 							$author_can_exec_php = $this->cap_cache[$id];
@@ -150,14 +150,14 @@ class php_execution
 							$this->cap_cache[$id] = $author_can_exec_php;
 						}
 					}
-					
+
 					if( !$this->current_user_can_exec_php && $author_can_exec_php )
 					{
 						$allcaps['edit_others_posts'] = false;
 						$allcaps['edit_others_pages'] = false;
 					}
 					break;
-				
+
 				// The following 2 cases are if checked always checked together with the above ones !?!?
 				// I've checked this for editing posts and pages in the general dialog.
 				// As !!very wisely!! no authorID or postID is provided with a cap check for
@@ -166,7 +166,7 @@ class php_execution
 				#case($args[0] == 'edit_others_posts'):
 				#case($args[0] == 'edit_others_pages'):
 					#break;
-			
+
 			}
 		}
 		return $allcaps;
@@ -181,7 +181,7 @@ class php_execution
 	}
 
 	/**
-	 * admin_print_scripts action. 
+	 * admin_print_scripts action.
 	 * Used to add the baseURL for the plugin.
 	 * Bug fix for cases where the WP_PLUGIN directory does not reside at its usual place.
 	 *
@@ -200,7 +200,7 @@ class php_execution
 		$content['phpExecution'] = PHP_EXECUTION_BASE_URL . '/assets/editor_plugin.js';
 		return $content;
 	}
-	
+
 	/**
 	 * prepare html data for editor
 	*/
@@ -215,13 +215,17 @@ class php_execution
 		// b) wp_htmledit_pre (wp-includes/formatting.php [1649]) = when tinymce is INACTIVE
 		if(wp_default_editor() != 'html')
 		{
-			$content = preg_replace(
-				'#<\?php([\s\S]*?)\?>#ie', 
-				'\'<img src="\' . PHP_EXECUTION_BASE_URL . \'/assets/trans.gif" class="mceWpPHP mceItemNoResize" title="php" alt="\' . base64_encode(stripslashes(\'\\0\')) . \'" />\'',
+			$content = preg_replace_callback(
+				'#<\?php([\s\S]*?)\?>#i',
+				function($m){
+					return '<img src="' . PHP_EXECUTION_BASE_URL . '/assets/trans.gif" class="mceWpPHP mceItemNoResize" title="php" alt="' . base64_encode(stripslashes($m[0])) . '" />';
+				},
 				$content
 			);
 		}
-		
+
+
+
 		return $content;
 	}
 
@@ -230,35 +234,35 @@ class php_execution
 	 * will execute or remove php code in/from post
 	*/
 	function action_the_content($content)
-	{	
+	{
 		return (strpos($content,'<?php') === false) // fast(er) precheck if post contains php
 		?	$content
 		: 	($this->php_execution_allowed())
 			? $this->execute_php($content)
 			: preg_replace('#<\?php([\s\S]*?)\?>#','',$content);
 	}
-	
+
 	/**
 	 * check if execution of php code is allowed
 	*/
 	function php_execution_allowed()
 	{
 		global $post;
-		
+
 		if (isset($post) && isset($post->post_author))
 		{
 			$id = $post->post_author;
-			
+
 			if(isset($this->cap_cache[$id])) return $this->cap_cache[$id];
-			
+
 			$user = new WP_User($id);
 			$perm = $user->has_cap(PHP_EXECUTION_CAPABILITY);
-			
+
 			// wp hook integrated here
 			$perm = apply_filters('php_execution_allowed',$perm,$user);
-			
+
 			$this->cap_cache[$id] = $perm;
-			
+
 			return $perm;
 		}
 		return false;
